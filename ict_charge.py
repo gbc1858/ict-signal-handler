f6cbad4d41ec1b3f397ca7a7aa9451a1f023925d
gbc1858 <scientisttomato@gmail.com>
2020-03-04 23:09:49 -0600

    change plot func

--- ict_charge.py
+++ ict_charge.py
@@ -36,7 +36,7 @@ class ProcessIctSignal:
                 try:
                     float(lines[i][0])
                     ans = True
-                except:
+                except ValueError:
                     ans = False
 
                 if ans is True:
@@ -45,8 +45,8 @@ class ProcessIctSignal:
 
         self.volt_list_all = np.reshape(volts_ls_all, (frame_number, num_data)).astype(np.float)
         self.time_list_all = np.reshape(time_ls_all, (frame_number, num_data)).astype(np.float)
-        return self.volt_list_all, self.time_list_all
 
+    # TODO: add exceptions
     # def get_ict_raw_data(self):
     #     if not self.volt_list_all or self.time_list_all:
     #         raise exceptions.IctDataError("Not able to get the ICT raw data.")
@@ -59,27 +59,26 @@ class ProcessIctSignal:
         for i in range(len(self.volt_list_all)):
             offset_single_frame = np.mean(self.volt_list_all[i][:settings.AVERAGE_NUMBER])
             self.offset.append(offset_single_frame)
-        return self.offset
 
     def integration_step(self):
         # Get time step for integration.
         for i in range(len(self.time_list_all)):
             dt_single_frame = abs(self.time_list_all[i][1] - self.time_list_all[i][0])
             self.dt.append(dt_single_frame)
-        return self.dt
 
     def get_ict_charge(self):
         charge_list_all = []
-        offset = self.get_volt_offset()
-        dt = self.integration_step()
-        volt_w_offset = [self.volt_list_all[i] - offset[i] for i in range(len(offset))]
+        self.get_volt_offset()
+        self.integration_step()
+        volt_w_offset = [self.volt_list_all[i] - self.offset[i] for i in range(len(self.offset))]
 
         for i in range(len(volt_w_offset)):
-            charge = simps(y=np.array(volt_w_offset[i]), dx=float(dt[i]))
-            charge_list_all.append(charge * 10 ** 12 / settings.ICT_CALIBRATION_FACTOR)    # charge in pC
+            charge = simps(y=np.array(volt_w_offset[i]), dx=float(self.dt[i]))
+            charge_list_all.append(charge * 10 ** 12 / settings.ICT_CALIBRATION_FACTOR)  # charge in pC
 
         # remove highest a few and lowest a few results.
-        desired_charges = sorted(charge_list_all)[settings.ICT_CUT_LOW_BOUNDARY: (len(charge_list_all) - settings.ICT_CUT_HIGH_BOUNDARY)]
+        desired_charges = sorted(charge_list_all)[
+                          settings.ICT_CUT_LOW_BOUNDARY: (len(charge_list_all) - settings.ICT_CUT_HIGH_BOUNDARY)]
         print('min charge  =>', np.max(desired_charges))
         print('max charge =>', np.min(desired_charges))
         print('std => ', np.std(desired_charges))
@@ -88,13 +87,13 @@ class ProcessIctSignal:
         return abs(np.mean(desired_charges)), np.std(desired_charges)
 
     def plot_ict_data(self):
-        pdf = PdfPages(self.file.split('.')[0] + '_ICT_raw.pdf')
-        for i in range(len(self.volt_list_all)):
-            plt.plot(self.time_list_all[i], self.volt_list_all[i], c='b')
-            plt.axhline(self.offset[i], c='darkorange', ls=':')
-            plt.xlabel('Time (s)')
-            plt.ylabel('Voltage (V)')
-            plt.title(self.file + "\nframe# %.1f (Offest = %.5f)" % (i + 1, self.offset[i]))
-            pdf.savefig(bbox_inches='tight')
-            plt.close()
-        pdf.close()
+        with PdfPages(self.file.split('.')[0] + '_ICT_raw.pdf') as pdf:
+            for i in range(len(self.volt_list_all)):
+                plt.plot(self.time_list_all[i], self.volt_list_all[i], c='b')
+                plt.axhline(self.offset[i], c='darkorange', ls=':')
+                plt.xlabel('Time (s)')
+                plt.ylabel('Voltage (V)')
+                plt.title(self.file + "\nframe# %.1f (Offest = %.5f)" % (i + 1, self.offset[i]))
+                pdf.savefig(bbox_inches='tight')
+                plt.close()
+
